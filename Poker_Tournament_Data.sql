6b4a1685b38dab98dd1a0cbbc4a19b4e9e77723b
alex giovannini <alexgiovannini@alexs-Air.lan>
2023-01-25 11:50:31 -0800

    Created SQL file

---
+++ Poker_Tournament_Data.sql
@@ -0,0 +1,9 @@
+-- CREATE DATABASE poker_tournaments;
+
+SELECT * FROM poker_tournaments.cleaned_poker_data;
+
+-- Locations with the best Prize Pool Ratio
+
+-- Locations with the highest buy in tournaments
+
+-- 
